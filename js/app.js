064626ea16ea425b68adefe813271e9dd96835f3
Jasczlv <ianis.cozlov@gmail.com>
2024-04-02 14:48:26 +0200

    ADD esercizio

--- js/app.js
+++ js/app.js
@@ -1,14 +1,43 @@
+// Descrizione:
+// Rifare l’esercizio della to do list.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// text, una stringa che indica il testo del todo
+// done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// NON perdetevi a fare troppi abbellimenti con il css all’inizio, ne a gestire layout particolari. Abbellirete tutto alla fine.
+// Scrivete solo il CSS necessario a completare le milestone
+// MILESTONE 1
+// Stampare all’interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una “x”:
+// cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante “aggiungi”:
+// cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      todoList: {
-        todo: {
-          text: "Fare esercizio",
-          done: false,
-        },
-      },
+      todo: [],
+      inputValue: "",
     };
   },
-}).mount(App);
+  methods: {
+    pushTodo() {
+      this.todo.push({
+        text: this.inputValue,
+        done: false,
+      });
+      this.inputValue = "";
+    },
+    isTrue(obj) {
+      if (obj.done === false) {
+        obj.done = true;
+      } else if (obj.done === true) {
+        obj.done = false;
+      }
+    },
+  },
+}).mount("#app");
